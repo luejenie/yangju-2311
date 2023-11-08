09672ac2ea41e07993f94dea874ad5d3a6613f3d
lej <luejenie@gmail.com>
2023-11-08 21:08:53 +0900

    lej

--- 모여라양주.py
+++ 모여라양주.py
@@ -3,7 +3,8 @@ from datetime import datetime
 now = datetime.now()
 
 
-
+print("---------------------------------------")
 print("안녕하세요. 이은지입니다. 첫글이에요.")
-print("모여라 양주인")
+print("pull -> commit -> push")
 print("작성시간:", now.strftime('%Y-%m-%D %H:%M:%S'))
+print("---------------------------------------")
